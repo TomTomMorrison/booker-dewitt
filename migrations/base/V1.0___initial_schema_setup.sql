84ec58b126b219fbfb29304dd300b91beb0ebf34
Tom Morrison <tom.morrison@unosquare.com>
2023-04-20 11:20:03 +0100

    Amended ERD & flyway scripts

--- migrations/base/V1.0___initial_schema_setup.sql
+++ migrations/base/V1.0___initial_schema_setup.sql
@@ -1,3 +1,5 @@
+SET datestyle = dmy;
+
 create table if not exists address
 (
     id serial not null
@@ -7,10 +9,10 @@ create table if not exists address
     postcode varchar (10) not null
 );
 
-create table if not exists nursery
+create table if not exists app_user
 (
     id serial not null
-        constraint nursery_pk primary key,
+        constraint user_pk primary key,
     name varchar(255) not null,
     email varchar(255) not null,
     password varchar (50) not null,
@@ -19,16 +21,20 @@ create table if not exists nursery
         constraint address_id_fk references address
 );
 
-create table if not exists guardian
+create table if not exists app_user_nursery
 (
     id serial not null
-        constraint guardian_pk primary key,
-    name varchar(255) not null,
-    email varchar(255) not null,
-    password varchar (50) not null,
-    contact_number varchar(255) not null,
-    address_id integer not null
-        constraint address_id_fk references address
+        constraint app_user_nursery_pk primary key,
+    app_user_id integer not null
+        constraint app_user_id_fk references app_user
+);
+
+create table if not exists app_user_guardian
+(
+    id serial not null
+        constraint app_user_guardian_pk primary key,
+    app_user_id integer not null
+        constraint app_user_id_fk references app_user
 );
 
 create table if not exists child
@@ -36,13 +42,13 @@ create table if not exists child
     id serial not null
         constraint child_pk primary key,
     name varchar(255) not null,
-    dob varchar(20) not null
+    dob timestamp not null default current_timestamp
 );
 
 create table if not exists guardian_child
 (
-    guardian_id integer not null
-        constraint guardian_id_fk references guardian,
+    app_user_guardian_id integer not null
+        constraint app_user_guardian_id_fk references app_user_guardian,
     child_id integer not null
         constraint child_id_fk references child
 );
@@ -54,13 +60,13 @@ create table if not exists status
     status varchar(20) not null
 );
 
-create table if not exists nursery_date_available
+create table if not exists available_dates
 (
     id serial not null
-        constraint nursery_date_available_pk primary key,
-    date varchar(20) not null,
-    nursery_id integer not null
-        constraint nursery_id_fk references nursery,
+        constraint available_dates_pk primary key,
+    available_date_time timestamp not null default current_timestamp,
+    app_user_nursery_id integer not null
+        constraint app_user_nursery_id_fk references app_user_nursery,
     status_id integer not null
         constraint status_id_fk references status
 );
@@ -69,11 +75,11 @@ create table if not exists nursery_child_date_available_request
 (
     id serial not null
         constraint nursery_child_date_available_request_pk primary key,
-    date varchar(20) not null,
+    request_date_time timestamp not null default current_timestamp,
     child_id integer not null
         constraint child_id_fk references child,
-    nursery_date_available_id integer not null
-        constraint nursery_id_fk references nursery_date_available,
+    available_dates_id integer not null
+        constraint available_dates_id_fk references available_dates,
     status_id integer not null
         constraint status_id_fk references status
 );
--- migrations/local-data-seed/V1000.1___local_data_seed.sql
+++ migrations/local-data-seed/V1000.1___local_data_seed.sql
@@ -1,57 +1,58 @@
+SET timezone = 'UTC';
+SET datestyle = dmy;
+
 insert into address(address, city, postcode) values ('1 New Street', 'Newark', 'SW2 9YH');
 insert into address(address, city, postcode) values ('2 Roman Lane', 'Sicily', 'IT2 7GP');
 insert into address(address, city, postcode) values ('3 Hells Kitchen', 'New York', 'HG2 4FL');
 insert into address(address, city, postcode) values ('4 Spanish Harlem', 'San Juan', 'TR4 9UI');
+insert into address(address, city, postcode) values ('2 New Street', 'Newark', 'SW2 9YH');
+insert into address(address, city, postcode) values ('3 Roman Lane', 'Sicily', 'IT2 7GP');
+insert into address(address, city, postcode) values ('4 Hells Kitchen', 'New York', 'HG2 4FL');
+insert into address(address, city, postcode) values ('5 Spanish Harlem', 'San Juan', 'TR4 9UI');
 
-insert into nursery(name, email, password, contact_number, address_id)
+insert into app_user(name, email, password, contact_number, address_id)
     values('Nursery 1', 'nursery1@mail.com', 'Password123', '02880576923', 1);
-insert into nursery(name, email, password, contact_number, address_id)
+insert into app_user(name, email, password, contact_number, address_id)
     values('Nursery 2', 'nursery2@mail.com', 'Password123', '02880198564', 2);
-insert into nursery(name, email, password, contact_number, address_id)
+insert into app_user(name, email, password, contact_number, address_id)
     values('Nursery 3', 'nursery3@mail.com', 'Password123', '02880340957', 3);
-insert into nursery(name, email, password, contact_number, address_id)
+insert into app_user(name, email, password, contact_number, address_id)
     values('Nursery 4', 'nursery4@mail.com', 'Password123', '02880076454', 4);
+insert into app_user(name, email, password, contact_number, address_id)
+    values('Tony Soprano', 'tsoprano@mail.com', 'Password123', '0975839284', 5);
+insert into app_user(name, email, password, contact_number, address_id)
+    values('Vito Corleone', 'vcorleone@mail.com', 'Password123', '6985769804', 6);
+insert into app_user(name, email, password, contact_number, address_id)
+    values('Henry Hill', 'hhill@mail.com', 'Password123', '9867433982', 7);
+insert into app_user(name, email, password, contact_number, address_id)
+    values('Carlito Brigante', 'cbrigante@mail.com', 'Password123', '4783989750', 8);
 
-insert into guardian(name, email, password, contact_number, address_id)
-    values('Tony Soprano', 'tsoprano@mail.com', 'Password123', '0975839284', 1);
-insert into guardian(name, email, password, contact_number, address_id)
-    values('Vito Corleone', 'vcorleone@mail.com', 'Password123', '6985769804', 2);
-insert into guardian(name, email, password, contact_number, address_id)
-    values('Henry Hill', 'hhill@mail.com', 'Password123', '9867433982', 3);
-insert into guardian(name, email, password, contact_number, address_id)
-    values('Carlito Brigante', 'cbrigante@mail.com', 'Password123', '4783989750', 4);
+insert into app_user_nursery(app_user_id) values (1);
+insert into app_user_nursery(app_user_id) values (2);
+insert into app_user_nursery(app_user_id) values (3);
+insert into app_user_nursery(app_user_id) values (4);
+insert into app_user_guardian(app_user_id) values (5);
+insert into app_user_guardian(app_user_id) values (6);
+insert into app_user_guardian(app_user_id) values (7);
+insert into app_user_guardian(app_user_id) values (8);
 
-insert into child(name, dob) values('Anthony Soprano', '12/02/2021');
-insert into child(name, dob) values('Michael Corleone', '31/06/2020');
-insert into child(name, dob) values('Harry Hill', '01/10/2020');
-insert into child(name, dob) values('Carl Brigante', '19/08/2019');
+insert into child(name, dob) values('Anthony Soprano', TIMESTAMP '12/02/2021');
+insert into child(name, dob) values('Michael Corleone', TIMESTAMP '30/06/2020');
+insert into child(name, dob) values('Harry Hill', TIMESTAMP '01/10/2020');
+insert into child(name, dob) values('Carl Brigante', TIMESTAMP '19/08/2019');
 
-insert into guardian_child(guardian_id, child_id) values(1, 1);
-insert into guardian_child(guardian_id, child_id) values(2, 2);
-insert into guardian_child(guardian_id, child_id) values(3, 3);
-insert into guardian_child(guardian_id, child_id) values(4, 4);
+insert into guardian_child(app_user_guardian_id, child_id) values(1, 1);
+insert into guardian_child(app_user_guardian_id, child_id) values(2, 2);
+insert into guardian_child(app_user_guardian_id, child_id) values(3, 3);
+insert into guardian_child(app_user_guardian_id, child_id) values(4, 4);
 
-insert into nursery_date_available(date, nursery_id, status_id) values('04/09/2022', 4, 1);
+insert into available_dates(available_date_time, app_user_nursery_id, status_id) values('04/09/2023 09:30:00', 4, 1);
 
-insert into nursery_child_date_available_request(date, child_id, nursery_date_available_id, status_id) values('20/08/2022', 1, 1, 3);
-insert into nursery_child_date_available_request(date, child_id, nursery_date_available_id, status_id) values('01/08/2022', 2, 1, 3);
-insert into nursery_child_date_available_request(date, child_id, nursery_date_available_id, status_id) values('11/08/2022', 3, 1, 3);
-insert into nursery_child_date_available_request(date, child_id, nursery_date_available_id, status_id) values('31/08/2022', 4, 1, 3);
-insert into nursery_child_date_available_request(date, child_id, nursery_date_available_id, status_id) values('20/08/2022', 1, 1, 3);
-insert into nursery_child_date_available_request(date, child_id, nursery_date_available_id, status_id) values('01/08/2022', 2, 1, 3);
-insert into nursery_child_date_available_request(date, child_id, nursery_date_available_id, status_id) values('11/08/2022', 3, 1, 3);
-insert into nursery_child_date_available_request(date, child_id, nursery_date_available_id, status_id) values('31/08/2022', 4, 1, 3);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+insert into nursery_child_date_available_request(request_date_time, child_id, available_dates_id, status_id) values(TIMESTAMP '20/08/2023 10:00:01', 1, 1, 3);
+insert into nursery_child_date_available_request(request_date_time, child_id, available_dates_id, status_id) values(TIMESTAMP '01/08/2023 11:00:01', 2, 1, 3);
+insert into nursery_child_date_available_request(request_date_time, child_id, available_dates_id, status_id) values(TIMESTAMP '11/08/2023 12:00:01', 3, 1, 3);
+insert into nursery_child_date_available_request(request_date_time, child_id, available_dates_id, status_id) values(TIMESTAMP '31/08/2023 13:00:01', 4, 1, 3);
+insert into nursery_child_date_available_request(request_date_time, child_id, available_dates_id, status_id) values(TIMESTAMP '20/08/2023 14:00:01', 1, 1, 3);
+insert into nursery_child_date_available_request(request_date_time, child_id, available_dates_id, status_id) values(TIMESTAMP '01/08/2023 15:00:01', 2, 1, 3);
+insert into nursery_child_date_available_request(request_date_time, child_id, available_dates_id, status_id) values(TIMESTAMP '11/08/2023 16:00:01', 3, 1, 3);
+insert into nursery_child_date_available_request(request_date_time, child_id, available_dates_id, status_id) values(TIMESTAMP '31/08/2023 17:00:01', 4, 1, 3);
